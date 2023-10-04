8a5eb5e1d21bedde4db1fb5472290d2cfe25775a
karimdib <karim.dib2001@gmail.com>
2023-10-04 15:39:14 +0200

    fix-cacl discount

--- js/app.js
+++ js/app.js
@@ -1,20 +1,16 @@
-let userKilometre = prompt('Quanti chilometri devi percorrere?');
+let userKilometre = prompt ('Quanti chilometri devi percorrere?');
 console.log(userKilometre);
 
-let price = (userKilometre * 0.21);
+let price = userKilometre * 0.21;
 console.log(price);
 
 let userAge = parseInt(prompt('Quanti anni hai?'));
 console.log(userAge);
 
-const discountJunior = (price * 20 / 100);
-
-const discountSenior = (price * 40 / 100);
-
-if(userAge < 18 ){
-    console.log('Il biglietto che pagherai è di' + ' ' + discountJunior + ' ' + 'euro');    
+if(userAge < 18){
+    console.log( price - (price * 20 / 100));    
 }else if(userAge > 65) {
-    console.log('Il biglietto che pagherai è di' + ' ' + discountSenior + ' ' + 'euro');
+    console.log(price - (price * 40 / 100));
 }else {
-    console.log ('Il biglietto che pagherai è di' + ' ' + price + ' ' + 'euro');
+    console.log (price);
 }
