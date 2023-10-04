89fe914b50ed62c6aad887c399d02308acff2129
karimdib <karim.dib2001@gmail.com>
2023-10-04 15:02:53 +0200

    Finish

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-let 
+let userKilometre = prompt('Quanti chilometri devi percorrere?');
+console.log(userKilometre);
+
+let price = (userKilometre * 0.21);
+console.log(price);
+
+let userAge = parseInt(prompt('Quanti anni hai?'));
+console.log(userAge);
+
+const discountJunior = (price * 20 / 100);
+
+const discountSenior = (price * 40 / 100);
+
+if(userAge < 18 ){
+    console.log('Il biglietto che pagherai è di' + ' ' + discountJunior + ' ' + 'euro');    
+}else if(userAge > 65) {
+    console.log('Il biglietto che pagherai è di' + ' ' + discountSenior + ' ' + 'euro');
+}else {
+    console.log ('Il biglietto che pagherai è di' + ' ' + price + ' ' + 'euro');
+}
